4bba4d1410422da66bf8b1cd0ae66668cc3c9084
JunhoKim <takis0112@gmail.com>
2018-04-10 16:39:02 +0900

    code

---
+++ dcgan/GAN.py
@@ -0,0 +1,296 @@
+import os
+import time
+import tensorflow as tf
+import numpy as np
+
+from ops import *
+from utils import *
+
+class GAN(object):
+    model_name = "GAN"     # name for checkpoint
+
+    def __init__(self, sess, args):
+        self.sess = sess
+        self.dataset = args.dataset
+        self.checkpoint_dir = args.checkpoint_dir
+        self.sample_dir = args.sample_dir
+        self.result_dir = args.result_dir
+        self.log_dir = args.log_dir
+
+        self.epoch = args.epoch
+        self.batch_size = args.batch_size
+        self.print_freq = args.print_freq
+        self.z_dim = args.z_dim  # dimension of noise-vector
+
+        self.sample_num = args.sample_num  # number of generated images to be saved
+        self.test_num = args.test_num
+
+        self.img_size = args.img_size
+
+        # train
+        self.learning_rate = 0.0002
+        self.beta1 = 0.5
+
+
+        if self.dataset == 'mnist' :
+            self.c_dim = 1
+            self.data_X = load_mnist(size=self.img_size)
+
+        elif self.dataset == 'ciar10' :
+            self.c_dim = 3
+            self.data_X = load_cifar10(size=self.img_size)
+
+        else :
+            self.c_dim = 3
+            self.data_X = load_data(dataset_name=self.dataset, size=self.img_size)
+
+
+        # get number of batches for a single epoch
+        self.num_batches = len(self.data_X) // self.batch_size
+
+    def discriminator(self, x, is_training=True, reuse=False):
+        with tf.variable_scope("discriminator", reuse=reuse):
+            ch = 32
+
+            # x = conv(x, channels=ch, kernel=5, stride=2, pad=2, scope='conv_0')
+            # x = lrelu(x)
+
+            for i in range(5):
+
+                # ch : 64 -> 128 -> 256 -> 512 -> 1024
+                # size : 32 -> 16 -> 8 -> 4 -> 2
+
+                x = conv(x, channels=ch*2, kernel=5, stride=2, pad=2, sn=True, scope='conv_'+str(i+1))
+                x = batch_norm(x, is_training, scope='batch_'+str(i))
+                x = lrelu(x)
+
+                ch = ch * 2
+
+            # [bs, 4, 4, 1024]
+
+            x = flatten(x)
+            x = fully_conneted(x, 1, sn=True)
+
+            return x
+
+    def generator(self, z, is_training=True, reuse=False):
+        with tf.variable_scope("generator", reuse=reuse):
+            ch = 1024
+
+            x = fully_conneted(z, ch)
+            x = relu(x)
+            x = tf.reshape(x, [-1, 1, 1, ch])
+
+            for i in range(5):
+
+                # ch : 512 -> 256 -> 128 -> 64 -> 32
+                # size : 2 -> 4 -> 8 -> 16 -> 32
+
+                x = deconv(x, channels=ch//2, kernel=5, stride=2, scope='deconv_'+str(i+1))
+                x = batch_norm(x, is_training, scope='batch_'+str(i))
+                x = relu(x)
+                ch = ch // 2
+
+            x = deconv(x, channels=self.c_dim, kernel=5, stride=2, scope='generated_image')
+            # [bs, 64, 64, c_dim]
+
+            x = tanh(x)
+
+            return x
+
+    def build_model(self):
+
+        """ Graph Input """
+        # images
+        self.inputs = tf.placeholder(tf.float32, [self.batch_size, self.img_size, self.img_size, self.c_dim], name='real_images')
+
+        # noises
+        self.z = tf.placeholder(tf.float32, [self.batch_size, self.z_dim], name='z')
+
+        """ Loss Function """
+        # output of D for real images
+        D_real_logits = self.discriminator(self.inputs)
+
+        # output of D for fake images
+        G = self.generator(self.z)
+        D_fake_logits = self.discriminator(G, reuse=True)
+
+        # get loss for discriminator
+        self.d_loss = discriminator_loss(real=D_real_logits, fake=D_fake_logits)
+
+        # get loss for generator
+        self.g_loss = generator_loss(fake=D_fake_logits)
+
+        """ Training """
+        # divide trainable variables into a group for D and a group for G
+        t_vars = tf.trainable_variables()
+        d_vars = [var for var in t_vars if 'discriminator' in var.name]
+        g_vars = [var for var in t_vars if 'generator' in var.name]
+
+        # optimizers
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+            self.d_optim = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).minimize(self.d_loss, var_list=d_vars)
+            self.g_optim = tf.train.AdamOptimizer(self.learning_rate, beta1=self.beta1).minimize(self.g_loss, var_list=g_vars)
+
+        """" Testing """
+        # for test
+        self.fake_images = self.generator(self.z, is_training=False, reuse=True)
+
+        """ Summary """
+        self.d_sum = tf.summary.scalar("d_loss", self.d_loss)
+        self.g_sum = tf.summary.scalar("g_loss", self.g_loss)
+
+
+
+    def train(self):
+
+        # initialize all variables
+        tf.global_variables_initializer().run()
+
+        # graph inputs for visualize training results
+        self.sample_z = np.random.uniform(-1, 1, size=(self.batch_size , self.z_dim))
+
+        # saver to save model
+        self.saver = tf.train.Saver()
+
+        # summary writer
+        self.writer = tf.summary.FileWriter(self.log_dir + '/' + self.model_dir, self.sess.graph)
+
+        # restore check-point if it exits
+        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+        if could_load:
+            start_epoch = (int)(checkpoint_counter / self.num_batches)
+            start_batch_id = checkpoint_counter - start_epoch * self.num_batches
+            counter = checkpoint_counter
+            print(" [*] Load SUCCESS")
+        else:
+            start_epoch = 0
+            start_batch_id = 0
+            counter = 1
+            print(" [!] Load failed...")
+
+        # loop for epoch
+        start_time = time.time()
+        for epoch in range(start_epoch, self.epoch):
+            # get batch data
+            for idx in range(start_batch_id, self.num_batches):
+                batch_images = self.data_X[idx*self.batch_size : (idx+1)*self.batch_size]
+
+                batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_dim])
+
+                train_feed_dict = {
+                    self.inputs : batch_images,
+                    self.z : batch_z
+                }
+
+                # update D network
+                _, summary_str, d_loss = self.sess.run([self.d_optim, self.d_sum, self.d_loss], feed_dict=train_feed_dict)
+                self.writer.add_summary(summary_str, counter)
+
+                # update G network
+                _, summary_str, g_loss = self.sess.run([self.g_optim, self.g_sum, self.g_loss], feed_dict=train_feed_dict)
+                self.writer.add_summary(summary_str, counter)
+
+                # display training status
+                counter += 1
+                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                      % (epoch, idx, self.num_batches, time.time() - start_time, d_loss, g_loss))
+
+                # save training results for every 300 steps
+                if np.mod(counter, self.print_freq) == 0:
+                    samples = self.sess.run(self.fake_images, feed_dict={self.z: self.sample_z})
+                    tot_num_samples = min(self.sample_num, self.batch_size)
+                    manifold_h = int(np.floor(np.sqrt(tot_num_samples)))
+                    manifold_w = int(np.floor(np.sqrt(tot_num_samples)))
+
+                    sample_dir = os.path.join(self.sample_dir, self.model_dir)
+                    check_folder(sample_dir)
+
+                    save_images(samples[:manifold_h * manifold_w, :, :, :],
+                                [manifold_h, manifold_w],
+                                './' + sample_dir + '/' + self.model_name + '_train_{:02d}_{:04d}.png'.format(epoch, idx))
+
+            # After an epoch, start_batch_id is set to zero
+            # non-zero value is only for the first epoch after loading pre-trained model
+            start_batch_id = 0
+
+            # save model
+            self.save(self.checkpoint_dir, counter)
+
+            # show temporal results
+            # self.visualize_results(epoch)
+
+        # save model for final step
+        self.save(self.checkpoint_dir, counter)
+
+    def visualize_results(self, epoch):
+        tot_num_samples = min(self.sample_num, self.batch_size)
+        image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
+
+        """ random condition, random noise """
+
+        z_sample = np.random.uniform(-1, 1, size=(self.batch_size, self.z_dim))
+
+        samples = self.sess.run(self.fake_images, feed_dict={self.z: z_sample})
+
+        sample_dir = os.path.join(self.sample_dir, self.model_dir)
+        check_folder(sample_dir)
+
+        save_images(samples[:image_frame_dim * image_frame_dim, :, :, :], [image_frame_dim, image_frame_dim],
+                    sample_dir + '/' + self.model_name + '_epoch%03d' % epoch + '_test_all_classes.png')
+
+    @property
+    def model_dir(self):
+        return "{}_{}_{}_{}".format(
+            self.model_name, self.dataset, self.batch_size, self.z_dim)
+
+    def save(self, checkpoint_dir, step):
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, self.model_name+'.model'), global_step=step)
+
+    def load(self, checkpoint_dir):
+        import re
+        print(" [*] Reading checkpoints...")
+        checkpoint_dir = os.path.join(checkpoint_dir, self.model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            counter = int(next(re.finditer("(\d+)(?!.*\d)",ckpt_name)).group(0))
+            print(" [*] Success to read {}".format(ckpt_name))
+            return True, counter
+        else:
+            print(" [*] Failed to find a checkpoint")
+            return False, 0
+
+    def test(self):
+        tf.global_variables_initializer().run()
+
+        self.saver = tf.train.Saver()
+        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
+        result_dir = os.path.join(self.result_dir, self.model_dir)
+        check_folder(result_dir)
+
+        if could_load:
+            print(" [*] Load SUCCESS")
+        else:
+            print(" [!] Load failed...")
+
+        tot_num_samples = min(self.sample_num, self.batch_size)
+        image_frame_dim = int(np.floor(np.sqrt(tot_num_samples)))
+
+        """ random condition, random noise """
+
+        for i in range(self.test_num) :
+            z_sample = np.random.uniform(-1, 1, size=(self.batch_size, self.z_dim))
+
+            samples = self.sess.run(self.fake_images, feed_dict={self.z: z_sample})
+
+            save_images(samples[:image_frame_dim * image_frame_dim, :, :, :],
+                        [image_frame_dim, image_frame_dim],
+                        result_dir + '/' + self.model_name + '_test_all_classes_{}.png'.format(i))
---
+++ dcgan/ops.py
@@ -0,0 +1,236 @@
+import tensorflow as tf
+import tensorflow.contrib as tf_contrib
+from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.contrib.layers import variance_scaling_initializer
+
+
+weight_init = tf.random_normal_initializer(mean=0.0, stddev=0.02)
+# weight_init = xavier_initializer()
+# weight_init = variance_scaling_initializer()
+
+
+# weight_regularizer = tf_contrib.layers.l2_regularizer(scale=0.0001)
+weight_regularizer = None
+
+# pad = (k-1) // 2 = SAME !
+# output = ( input - k + 1 + 2p ) // s
+
+def conv(x, channels, kernel=4, stride=2, pad=0, pad_type='zero', use_bias=True, sn=False, scope='conv_0'):
+    with tf.variable_scope(scope):
+        if pad_type == 'zero' :
+            x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]])
+        if pad_type == 'reflect' :
+            x = tf.pad(x, [[0, 0], [pad, pad], [pad, pad], [0, 0]], mode='REFLECT')
+
+        if sn :
+            w = tf.get_variable("kernel", shape=[kernel, kernel, x.get_shape()[-1], channels], initializer=weight_init,
+                                regularizer=weight_regularizer)
+            bias = tf.get_variable("bias", [channels], initializer=tf.constant_initializer(0.0))
+            x = tf.nn.conv2d(input=x, filter=spectral_norm(w),
+                             strides=[1, stride, stride, 1], padding='VALID')
+            if use_bias :
+                x = tf.nn.bias_add(x, bias)
+
+        else :
+            x = tf.layers.conv2d(inputs=x, filters=channels,
+                                 kernel_size=kernel, kernel_initializer=weight_init,
+                                 kernel_regularizer=weight_regularizer,
+                                 strides=stride, use_bias=use_bias)
+
+
+
+        return x
+
+
+def deconv(x, channels, kernel=4, stride=2, use_bias=True, scope='deconv_0'):
+    with tf.variable_scope(scope):
+        x = tf.layers.conv2d_transpose(inputs=x, filters=channels,
+                                       kernel_size=kernel, kernel_initializer=weight_init, kernel_regularizer=weight_regularizer,
+                                       strides=stride, padding='SAME', use_bias=use_bias)
+
+        return x
+
+def max_pooling(x, kernel=2, stride=2) :
+    return tf.layers.max_pooling2d(x, pool_size=kernel, strides=stride)
+
+def avg_pooling(x, kernel=2, stride=2) :
+    return tf.layers.average_pooling2d(x, pool_size=kernel, strides=stride)
+
+def global_avg_pooling(x):
+    """
+    Incoming Tensor shape must be 4-D
+    """
+    gap = tf.reduce_mean(x, axis=[1, 2])
+    return gap
+
+def fully_conneted(x, units, use_bias=True, sn=False, scope='fully_0'):
+    with tf.variable_scope(scope):
+        x = flatten(x)
+        shape = x.get_shape().as_list()
+        channels = shape[-1]
+
+        if sn :
+            w = tf.get_variable("kernel", [channels, units], tf.float32,
+                                     initializer=weight_init, regularizer=weight_regularizer)
+            if use_bias :
+                bias = tf.get_variable("bias", [units],
+                                       initializer=tf.constant_initializer(0.0))
+
+                x = tf.matmul(x, spectral_norm(w)) + bias
+            else :
+                x = tf.matmul(x, spectral_norm(w))
+
+        else :
+            x = tf.layers.dense(x, units=units, kernel_initializer=weight_init, kernel_regularizer=weight_regularizer, use_bias=use_bias)
+
+        return x
+
+
+def flatten(x) :
+    return tf.layers.flatten(x)
+
+
+def lrelu(x, alpha=0.2):
+    # pytorch alpha is 0.01
+    return tf.nn.leaky_relu(x, alpha)
+
+
+def relu(x):
+    return tf.nn.relu(x)
+
+
+def sigmoid(x):
+    return tf.sigmoid(x)
+
+
+def tanh(x):
+    return tf.tanh(x)
+
+
+def swish(x):
+    return x * sigmoid(x)
+
+def discriminator_loss(real, fake):
+    real_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(real), logits=real))
+    fake_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.zeros_like(fake), logits=fake))
+    loss = real_loss + fake_loss
+
+    return loss
+
+
+def generator_loss(fake):
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(fake), logits=fake))
+
+    return loss
+
+
+
+def batch_norm(x, is_training=True, scope='batch_norm'):
+    return tf_contrib.layers.batch_norm(x,
+                                        decay=0.9, epsilon=1e-05,
+                                        center=True, scale=True, updates_collections=None,
+                                        is_training=is_training, scope=scope)
+
+    # return tf.layers.batch_normalization(x, momentum=0.99, epsilon=1e-05, center=True, scale=True, training=is_training, name=scope)
+
+
+
+def instance_norm(x, scope='instance_norm'):
+    return tf_contrib.layers.instance_norm(x,
+                                           epsilon=1e-05,
+                                           center=True, scale=True,
+                                           scope=scope)
+
+def layer_norm(x, scope='layer_norm') :
+    return tf_contrib.layers.layer_norm(x,
+                                        center=True, scale=True,
+                                        scope=scope)
+
+def group_norm(x, G=32, eps=1e-5, scope='group_norm') :
+    with tf.variable_scope(scope) :
+        N, H, W, C = x.get_shape().as_list()
+        G = min(G, C)
+
+        x = tf.reshape(x, [N, H, W, G, C // G])
+        mean, var = tf.nn.moments(x, [1, 2, 4], keep_dims=True)
+        x = (x - mean) / tf.sqrt(var + eps)
+
+        gamma = tf.get_variable('gamma', [1, 1, 1, C],
+                                initializer=tf.constant_initializer(1.0))
+        beta = tf.get_variable('beta', [1, 1, 1, C],
+                               initializer=tf.constant_initializer(0.0))
+        # gamma = tf.reshape(gamma, [1, 1, 1, C])
+        # beta = tf.reshape(beta, [1, 1, 1, C])
+
+        x = tf.reshape(x, [N, H, W, C]) * gamma + beta
+
+    return x
+
+
+def l2_norm(v, eps=1e-12):
+    return v / (tf.reduce_sum(v ** 2) ** 0.5 + eps)
+
+
+def spectral_norm(w, iteration=1, update_collection=tf.GraphKeys.UPDATE_OPS):
+    first_call = not tf.get_variable_scope().reuse
+
+    w_shape = w.shape.as_list()
+    w = tf.reshape(w, [-1, w_shape[-1]])
+
+    u_hat, v_hat = None, None
+
+    u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
+    u_iter = u
+    for i in range(iteration):
+        """
+
+        power iteration
+        Usually iteration = 1 will be enough
+
+        """
+        v_ = tf.matmul(u_iter, tf.transpose(w))
+        v_hat = l2_norm(v_)
+
+        u_ = tf.matmul(v_hat, w)
+        u_hat = l2_norm(u_)
+        u_iter = u_hat
+
+    sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
+    w_norm = w / sigma
+
+    if update_collection is None:
+        """
+
+        Setting update_collection to None will make u being updated every W execution. This maybe undesirable
+        Please consider using a update collection instead.
+        default update_collection is tf.GraphKeys.UPDATE_OPS
+
+        """
+        with tf.control_dependencies([u.assign(u_hat)]):
+            w_norm = tf.reshape(w_norm, w_shape)
+    else:
+        if first_call:
+            """
+
+            if GAN : 
+                Discriminator and Generator will call for real image
+                If fake image, it can not pass here
+
+            """
+            tf.add_to_collection(update_collection, u.assign(u_hat))
+
+        w_norm = tf.reshape(w_norm, w_shape)
+
+    return w_norm
+
+
+def L1_loss(x, y):
+    loss = tf.reduce_mean(tf.abs(x - y))
+
+    return loss
+
+def L2_loss(x, y):
+    loss = tf.reduce_mean(tf.square(x - y))
+
+    return loss
+
