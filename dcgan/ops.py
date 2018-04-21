790afc5e35494df40199a473912f3467d74fe77f
JunhoKim <takis0112@gmail.com>
2018-04-22 00:54:03 +0900

    modified

--- dcgan/ops.py
+++ dcgan/ops.py
@@ -180,34 +180,30 @@ def group_norm(x, G=32, eps=1e-5, scope='group_norm') :
 def l2_norm(v, eps=1e-12):
     return v / (tf.reduce_sum(v ** 2) ** 0.5 + eps)
 
-
 def spectral_norm(w, iteration=1):
     w_shape = w.shape.as_list()
     w = tf.reshape(w, [-1, w_shape[-1]])
 
-    u_hat, v_hat = None, None
-
     u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
 
+    u_hat = u
+    v_hat = None
     for i in range(iteration):
-
         """
-
         power iteration
         Usually iteration = 1 will be enough
-
         """
-        v_ = tf.matmul(u, tf.transpose(w))
+        v_ = tf.matmul(u_hat, tf.transpose(w))
         v_hat = l2_norm(v_)
 
         u_ = tf.matmul(v_hat, w)
         u_hat = l2_norm(u_)
-        u = u_hat
 
     sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
     w_norm = w / sigma
 
-    w_norm = tf.reshape(w_norm, w_shape)
+    with tf.control_dependencies([u.assign(u_hat)]):
+        w_norm = tf.reshape(w_norm, w_shape)
 
     return w_norm
 
