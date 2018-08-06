65218e8cc6916d24b49504c337981548685e1be1
Junho Kim (1993.01.12) <takis0112@gmail.com>
2018-08-06 11:49:25 +0900

    Update spectral_norm.py

--- spectral_norm.py
+++ spectral_norm.py
@@ -1,32 +1,34 @@
 import tensorflow as tf
 
-def l2_norm(v, eps=1e-12):
-    return v / (tf.reduce_sum(v ** 2) ** 0.5 + eps)
-
-
 def spectral_norm(w, iteration=1):
     w_shape = w.shape.as_list()
     w = tf.reshape(w, [-1, w_shape[-1]])
 
-    u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.truncated_normal_initializer(), trainable=False)
+    u = tf.get_variable("u", [1, w_shape[-1]], initializer=tf.random_normal_initializer(), trainable=False)
 
     u_hat = u
     v_hat = None
     for i in range(iteration):
+        
         """
         power iteration
         Usually iteration = 1 will be enough
         """
+        
         v_ = tf.matmul(u_hat, tf.transpose(w))
-        v_hat = l2_norm(v_)
+        v_hat = tf.nn.l2_normalize(v_)
 
         u_ = tf.matmul(v_hat, w)
-        u_hat = l2_norm(u_)
+        u_hat = tf.nn.l2_normalize(u_)
+
+    u_hat = tf.stop_gradient(u_hat)
+    v_hat = tf.stop_gradient(v_hat)
 
     sigma = tf.matmul(tf.matmul(v_hat, w), tf.transpose(u_hat))
-    w_norm = w / sigma
 
     with tf.control_dependencies([u.assign(u_hat)]):
+        w_norm = w / sigma
         w_norm = tf.reshape(w_norm, w_shape)
+
 
     return w_norm
